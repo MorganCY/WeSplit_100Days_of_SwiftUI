7ce90b78cd08d66fd4630d8c234a3c41526648fc
Morgan <nihao0705@gmail.com>
2022-11-01 23:22:41 +0800

    Reading text from the user with TextField

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+
+    let tipPercentages = [10, 15, 20, 0]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                // $ represents two-way binding
+                // replace text with value to implicate the double property
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "TWD"))
+                    .keyboardType(.decimalPad)
+            }
+
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "TWD"))
+            }
+        }
     }
 }
 
